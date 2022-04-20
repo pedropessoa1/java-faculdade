30429a8766a74e4b331527179a4952d4224c8404
Pedro Henrique <ph90305@gmail.com>
2022-04-20 10:57:36 -0300

    main

---
+++ atividades-sala/20-04/ex001/ex001/src/classes/calculomecc.java
@@ -0,0 +1,33 @@
+package classes;
+
+public class calculomecc {
+
+    public double altura, peso;
+
+    public double calculomc(){
+
+
+         double resulmc = (peso / Math.pow(altura, 2));
+
+        return resulmc ;
+
+    }
+
+   /* public double perimetro(){
+
+        double valorperimetro = (largura + largura + altura + altura );
+
+        return valorperimetro;
+    }
+    //                2           2
+    // raiz de  altura  + largura
+// Math.pow(altura, 2)        Math.sqrt()
+    public double diagonal(){
+
+        double valordiagonal = ( Math.sqrt( Math.pow(altura,2) + Math.pow(largura, 2)));
+
+        return valordiagonal;*/
+    }
+    
+    
+
